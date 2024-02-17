22e73528103309211ff1adac166e986a7c5507cc
Lean Kurt <leankurtescano@gmail.com>
2024-02-17 18:36:09 +0800

    new commit

--- my-vite-project/src/App.jsx
+++ my-vite-project/src/App.jsx
@@ -2,25 +2,30 @@ import { useReducer } from 'react';
 import './App.css';
 
 const reducer = (state, action) => {
-  switch (action.type) {
-    case 'buy_ingredients':
-      return { ...state, myMoney: state.myMoney - 100 };
-    case 'sell_ingredients':
-      return { ...state, myMoney: state.myMoney + 100 };
-    default:
-      return state;
+  if(action.type == 'remove_cart'){
+    if (state.quantity > 0) {
+      return {
+        ...state,
+        price: Math.max(0, state.price - 100),
+        quantity: Math.max(0, state.quantity - 1)
+      }
+    }
+  } else if(action.type == 'add_cart') {
+    return{...state, price: state.price + 100, quantity: state.quantity + 1};
   }
+  return state;
 };
 
 function App() {
-  const initialState = { myMoney: 0 };
+  const initialState = { price: 100 , quantity: 1};
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
-      <h1>Money: {state.myMoney}</h1>
-      <button onClick={() => dispatch({ type: 'buy_ingredients' })}>Buy ingredients</button>
-      <button onClick={() => dispatch({ type: 'sell_ingredients' })}>Sell ingredients</button>
+      <h1>price: {state.price}</h1>
+      <h1>quantity: {state.quantity}</h1>
+      <button onClick={() => dispatch({ type: 'remove_cart' })}>-</button>
+      <button onClick={() => dispatch({ type: 'add_cart' })}>+</button>
     </div>
   );
 }
